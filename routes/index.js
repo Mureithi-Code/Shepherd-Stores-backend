5832fe223a8e5d5a1580dbf6c2bde7e78d6a91d0
Mureithi-Code <darkknight505c@example.com>
2025-04-02 13:09:10 +0300

    admin prvlg done

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const authToken = require('../middleware/authToken')
 const userDetailsController = require('../controller/userDetails')
 const userLogout = require('../controller/userLogout')
 const allUsers = require('../controller/allUsers')
+const updateUser = require('../controller/updateUser')
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
@@ -16,6 +17,7 @@ router.get("/userLogout",userLogout)
 
 //admin panel 
 router.get("/all-user",authToken,allUsers)
+router.post("/update-user",authToken,updateUser)
 
 
 module.exports = router
