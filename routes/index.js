c800c33f9671c0f15164d4ec575034aad37c7b9c
Mureithi-Code <darkknight505c@example.com>
2025-04-02 15:22:18 +0300

    upload product

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const userDetailsController = require('../controller/userDetails')
 const userLogout = require('../controller/userLogout')
 const allUsers = require('../controller/allUsers')
 const updateUser = require('../controller/updateUser')
+const UploadProductController = require('../controller/uploadProduct')
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
@@ -18,6 +19,10 @@ router.get("/userLogout",userLogout)
 //admin panel 
 router.get("/all-user",authToken,allUsers)
 router.post("/update-user",authToken,updateUser)
+
+//product
+router.post("/upload-product",authToken,UploadProductController)
+
 
 
 module.exports = router
