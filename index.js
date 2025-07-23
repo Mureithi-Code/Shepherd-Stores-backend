09cfd2d71b1eafbeb5c0b1e3fcfb85d50ff428cc
Mureithi-Code <darkknight505c@example.com>
2025-07-23 13:31:16 +0300

    CORS config

--- index.js
+++ index.js
@@ -7,25 +7,21 @@ const router = require('./routes')
 
 
 const app = express()
-const allowedOrigins = process.env.FRONTEND_URL.split(',').map(origin => origin.trim())
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like curl, mobile apps)
-    if (!origin) return callback(null, true)
-    
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true)
-    } else {
-      console.error("CORS blocked:", origin)
-      return callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true
-}))
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://shepherd-stores.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}
 
-app.use(express.json({ limit: '20mb' }))
-app.use(express.urlencoded({ extended: true, limit: '20mb' }))
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions))
+
+app.use(express.json({ limit: "20mb" }))
+app.use(express.urlencoded({ extended: true, limit: "20mb" }))
 app.use(cookieParser())
 
 app.use("/api",router)
